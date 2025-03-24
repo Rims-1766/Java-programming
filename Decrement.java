b3e643078321d99ee7c8d9eebaa82979e911d6f7
Rims-1766 <rimsha1760548@gmail.com>
2025-03-24 20:57:56 +0530

    Add files via upload

---
+++ Decrement.java
@@ -0,0 +1,12 @@
+class Decremaent{
+	public static void main(String...args){
+
+	int a=20;
+	System.out.println(--a + a);
+ 	//pre decrement
+	System.out.println(a--+a);
+	//summation without method
+	a=20;
+	System.out.println(Integer.sum(--a,a));
+	}
+}

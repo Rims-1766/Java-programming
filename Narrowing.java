c82f9d522f395834719276e68c60892861f85534
Rims-1766 <rimsha1760548@gmail.com>
2025-03-24 20:50:55 +0530

    Add files via upload

---
+++ Narrowing.java
@@ -0,0 +1,12 @@
+class First{
+	public static void main(String...args[]){
+	double d=55.55;
+	float f=(float)d;
+	long  li=(long)f;
+	int i=(int)li;
+	short sh=(short)i;
+	byte b=(byte)sh;
+
+	System.out.println(d +"-"+ f+"-"+ li+"-"+ sh+"-"+ b);
+	}
+}

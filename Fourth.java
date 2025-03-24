1b25997bd7da1675260233971244e7e61572f780
Rims-1766 <rimsha1760548@gmail.com>
2025-03-24 20:58:55 +0530

    Add files via upload

---
+++ Fourth.java
@@ -0,0 +1,19 @@
+class Fourth{
+	int x=10;
+	 int y=20;
+	 static int result;
+	static{
+	result=x+y;
+	System.out.println(result);
+	result=x-y;
+	System.out.println(result);
+	result=x*y;
+	System.out.println(result);
+	result=x/y;
+	System.out.println(x/y);
+	}
+	public static void main(String...args){
+	Fourth = new Fourth();
+	Fourth();
+	}
+}

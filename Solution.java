8bd69eebd378b72bcaa4f760aeba069d2fe52bed
Rims-1766 <rimsha1760548@gmail.com>
2025-03-24 20:57:26 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Solution{
+	public static void main(String...args){
+	Scanner scan=new Scanner(System.in);
+	int a =scan.nextInt();
+	double d=scan.nextDouble();
+	String str=scan.nextLine();
+
+	System.out.println("my Int is :"+a);
+	System.out.println("my String is :"+str);
+	System.out.println("my Double is :"+d);
+	}
+}

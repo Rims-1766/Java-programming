a01731845beada4fbd627b298a5ae78d9c513180
rims.. <rimsha1760548@gmail.com>
2026-02-03 22:13:53 +0530

    Create L3P1.java

---
+++ L3P1.java
@@ -0,0 +1,14 @@
+public class L3P1{
+		static int view;
+		public static  void display(){
+			
+			System.out.println(L3P1.view);
+		}
+		int show(){
+			int view =1;
+			return L3P1.view;	
+
+		}
+		
+
+}

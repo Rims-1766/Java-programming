8d84259751b1da5d67880a1d6ef3e876cd7c1f13
rims.. <rimsha1760548@gmail.com>
2026-02-03 22:15:37 +0530

    Create L3P3.java

---
+++ L3P3.java
@@ -0,0 +1,23 @@
+class Animalmain(){
+	public static void main(String...args){
+		Animal obj=new Animal();
+		obj.Animal();
+		obj.makesound();
+	}
+		
+}
+class Animal{
+	public static void makesound(){
+		System.out.println("Animals make sounds");
+
+	}
+}
+public class cat extends makesound{
+		@Override
+		public void makesound(){
+			System.out.println("the cat is murmuring");
+			super.makesound();
+		}
+}
+
+
